121939e3ff78c2386bb82729ea695932af45b258
Rafidah Alias <48427970+rafalias@users.noreply.github.com>
2020-10-27 17:32:40 +0800

    Add files via upload

---
+++ build/precache-manifest.af86bed30eac1377384915192579c430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebc4262243f11eb327aec443273b675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe399e52fd51427a562",
+    "url": "/static/css/main.f9ec25cb.chunk.css"
+  },
+  {
+    "revision": "0cd6776838acceb28788",
+    "url": "/static/js/2.c6efc2fd.chunk.js"
+  },
+  {
+    "revision": "890fe60e51f18d9e8e0004117fd20dc3",
+    "url": "/static/js/2.c6efc2fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe399e52fd51427a562",
+    "url": "/static/js/main.cd89cc49.chunk.js"
+  },
+  {
+    "revision": "dd5ef10a7136abb7020b",
+    "url": "/static/js/runtime-main.4c9ea70c.js"
+  }
+]);
